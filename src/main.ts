0ab3ac21c5c465601ec2f72d3cfc813680ae29f7
ZRMYDYCG <547471919@qq.com>
2024-05-19 18:15:26 +0800

    feat add 路由配置 常量路由、路由模式、滚动行为

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
-
 import 'virtual:svg-icons-register' // svg 配置
 import globalComponent from '@/components/index.ts' // 引入自定义插件对象：注册整个项目的全局组件
 import '@/styles/index.scss' // 引入全局样式
+import router from './router/index.ts' // 路由
 
 const app = createApp(App)
-
 app.use(globalComponent) // 安装自定义插件
+app.use(router)
 app.mount('#app')
