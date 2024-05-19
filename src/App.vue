c4d95b8bdc7d6425d3d91cd3f6ac278e819ae2e8
ZRMYDYCG <547471919@qq.com>
2024-05-19 15:02:18 +0800

    test add mock 测试

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { onMounted } from "vue";
 
+onMounted(() => {
+  // 登录接口
+  axios({
+    url: '/api/user/login',
+    method: 'POST',
+    data: {
+      username: 'admin',
+      password: '123456789'
+    }
+  }).then(res => {
+    console.log(res)
+  })
+})
 </script>
 
 <template>
-  <div>我是App根组件</div>
-  <svg-icon name="phone"></svg-icon>
+
 </template>
 
 <style scoped lang="scss">
